3531bdcbfb1884f5044c0445c9208183af3b6a34
Guilherme Matheus Liesenfeld - ACERTA <guilherme.liesenfeld@acertapromotora.com.br>
2022-09-03 11:31:18 -0300

    New component

---
+++ src/components/Input/Input.style.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const Input = styled.div`
+   background-color: black;
+   color: white;
+   
+`
+
+export default Input
---
+++ src/components/atoms/Button/Button.style.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+   background-color: black;
+   color: white;
+   
+`
+
+export default Button
