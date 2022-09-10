dbdf961d9f3f73059fbf07e4115cc5f4b43c7bf5
Guilherme Matheus Liesenfeld - ACERTA <guilherme.liesenfeld@acertapromotora.com.br>
2022-09-10 11:03:40 -0300

    add base style

--- src/assets/styles/GlobalStyles.ts
+++ src/assets/styles/GlobalStyles.ts
@@ -1,4 +1,10 @@
 import {createGlobalStyle} from 'styled-components'
+import coffeeBean from '../images/coffee-beans.svg'
+
+import {colors} from '../variables'
+
+const {brown} = colors
+
 
 const GlobalStyle = createGlobalStyle`
   *{
@@ -6,6 +12,15 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
 }
+
+body{
+  background-color: ${brown};
+  background-image: url(${coffeeBean});
+  background-size: 600px;
+  height: 100vh;
+  
+}
 `
+
 
 export default GlobalStyle
---
+++ src/assets/variables.ts
@@ -0,0 +1,14 @@
+interface ColorsInterface {
+  brown: string
+  darkBrown: string
+  lightBrown: string
+  veryLightBrown: string
+
+}
+
+export const colors: ColorsInterface = {
+  brown: '#5c3015',
+  darkBrown: '#240f04',
+  lightBrown: '#9f5524',
+  veryLightBrown: '#bc906a',
+}
